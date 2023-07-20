df60cdeb026a2a244ea9fc3059be3098a23fb232
winterSteve25 <w1nter.ish3re@gmail.com>
2023-07-20 08:36:18 -0400

    fixed gui

--- src/main/java/com/github/wintersteve25/oniutils/common/contents/modules/items/gadgets/blueprint/ONIBlueprintGui.java
+++ src/main/java/com/github/wintersteve25/oniutils/common/contents/modules/items/gadgets/blueprint/ONIBlueprintGui.java
@@ -9,7 +9,6 @@ import com.github.wintersteve25.tau.layout.Layout;
 import com.github.wintersteve25.tau.layout.LayoutSetting;
 import com.github.wintersteve25.tau.renderer.ScreenUIRenderer;
 import com.github.wintersteve25.tau.theme.Theme;
-import com.github.wintersteve25.tau.utils.FlexSizeBehaviour;
 import com.github.wintersteve25.tau.utils.ItemRenderProvider;
 import com.github.wintersteve25.tau.utils.Pad;
 import com.github.wintersteve25.tau.utils.Size;
@@ -19,61 +18,78 @@ import net.minecraftforge.common.util.Lazy;
 
 import java.util.List;
 import java.util.Set;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
-public class ONIBlueprintGui implements UIComponent {
+public class ONIBlueprintGui extends DynamicUIComponent {
     
     private static final Lazy<List<BlueprintRecipe>> RECIPES = Lazy.of(() -> Minecraft.getInstance().level.getRecipeManager().getAllRecipesFor(ONIRecipes.BLUEPRINT_RECIPE_TYPE.get()));
     
+    private Set<BlueprintRecipe> recipesShown;
+    
+    private UIComponent categoryButtons() {
+        return new Row.Builder()
+            .withSpacing(2)
+            .build(
+                new Category("Base", RECIPES.get().stream().collect(Collectors.toSet()), recipes -> {
+                    recipesShown = recipes;
+                    rebuild();
+                }),
+                new Category("Power", RECIPES.get().stream().collect(Collectors.toSet()), recipes -> {
+                    recipesShown = recipes;
+                    rebuild();
+                }));
+    }
+    
     @Override
     public UIComponent build(Layout layout, Theme theme) {
+        if (recipesShown == null) {
+            return new Align.Builder()
+                .withVertical(LayoutSetting.END)
+                .build(new Padding(
+                    new Pad.Builder().left(2).bottom(2).build(),
+                    new Column.Builder()
+                        .withSpacing(2)
+                        .withAlignment(LayoutSetting.START)
+                        .build(
+                                new Sized(
+                                    Size.staticSize(200, 300),
+                                    new Container.Builder()
+                                        .noBackground()),
+                                categoryButtons()
+                        )));
+        }
+        
         return new Align.Builder()
             .withVertical(LayoutSetting.END)
             .build(new Padding(
                 new Pad.Builder().left(2).bottom(2).build(),
-                new Row.Builder()
+                new Column.Builder()
                     .withSpacing(2)
+                    .withAlignment(LayoutSetting.START)
                     .build(
-                        new Category("Base", RECIPES.get().stream().collect(Collectors.toSet())),
-                        new Category("Power", RECIPES.get().stream().collect(Collectors.toSet())))));
+                        new Sized(
+                            Size.staticSize(200, 300),
+                            new Container.Builder()
+                                .withChild(new ListView.Builder()
+                                    .withSpacing(2)
+                                    .build(recipesShown.stream().map(Building::new).collect(Collectors.toUnmodifiableList())))),
+                        categoryButtons()
+                    )));
     }
 
     public static void open() {
         Minecraft.getInstance().setScreen(new ScreenUIRenderer(new ONIBlueprintGui()));
     }
 
-    private static final class Category extends DynamicUIComponent {
-        private final String categoryName;
-        private final Set<BlueprintRecipe> recipes;
-
-        private boolean detailsShown;
-       
-        private Category(String categoryName, Set<BlueprintRecipe> recipes) {
-            this.categoryName = categoryName;
-            this.recipes = recipes;
-        }
-
+    private record Category(String categoryName, Set<BlueprintRecipe> recipes, Consumer<Set<BlueprintRecipe>> onActivate) implements UIComponent {
         @Override
         public UIComponent build(Layout layout, Theme theme) {
-            return new Column.Builder()
-                .withAlignment(LayoutSetting.START)
-                .build(
-                    new If(
-                        detailsShown,
-                        new Sized(
-                            Size.staticSize(200, 300),
-                            new Container.Builder()
-                                .withChild(new ListView.Builder()
-                                    .withSpacing(2)
-                                    .build(recipes.stream().map(Building::new).collect(Collectors.toUnmodifiableList()))))),
-                new Sized(
-                    Size.staticSize(80, 20),
-                    new Button.Builder()
-                        .withOnPress((btn) -> {
-                            detailsShown = !detailsShown;
-                            rebuild();
-                        })
-                        .build(new Center(new Text.Builder(categoryName)))));
+            return new Sized(
+                Size.staticSize(80, 20),
+                new Button.Builder()
+                    .withOnPress((btn) -> onActivate.accept(recipes))
+                    .build(new Center(new Text.Builder(categoryName))));
         }
     }
     
